d40749993a6a6f99c4b37a1841ca67bdaa407da7
Alexander Lazar <alexanderlazar@icloud.com>
2025-12-13 17:47:51 +0100

    Initial Commit

---
+++ Ball game/AppDelegate.swift
@@ -0,0 +1,24 @@
+//
+//  AppDelegate.swift
+//  Ball game
+//
+//  Created by Alexander Lazar on 13.12.25.
+//
+
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+    
+    
+}
