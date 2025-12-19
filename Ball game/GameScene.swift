59dc22b5e225cb5f90c954cf235167ab00b2d1df
Alexander Lazar <alexanderlazar@icloud.com>
2025-12-19 16:07:12 +0100

    Implement the game basics

--- Ball game/GameScene.swift
+++ Ball game/GameScene.swift
@@ -4,88 +4,129 @@
 //
 //  Created by Alexander Lazar on 13.12.25.
 //
-
+// events: mouseDown mouseDragged mouseUp keyDown
 import SpriteKit
-import GameplayKit
+import AppKit
 
 class GameScene: SKScene {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    var ball: SKShapeNode!
+    var indicator: SKShapeNode!
+    
+    var moveLeft: Bool = false
+    var moveRight: Bool = false
+    var moveUp: Bool = false
+    var moveDown: Bool = false
     
     override func didMove(to view: SKView) {
+        self.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        self.view?.window?.makeFirstResponder(self)
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        let radius: CGFloat = 20
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        ball = SKShapeNode(circleOfRadius: radius)
+        ball.fillColor = .red
+        ball.strokeColor = .clear
+        ball.position = CGPoint(x: 0, y: 0)
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
+        addChild(ball)
+        
+        let indicatorRadius: CGFloat = 10
+    
+        indicator = SKShapeNode(circleOfRadius: indicatorRadius)
+        indicator.fillColor = .green
+        indicator.strokeColor = .clear
+        indicator.position = CGPoint(x: 0, y: 0)
+        indicator.isHidden = true
+        addChild(indicator)
+        //let halfWidth = size.width / 2
+        //let halfHeight = size.height / 2
     }
-    
-    
-    func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
-    }
-    
-    func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func mouseDown(with event: NSEvent) {
-        self.touchDown(atPoint: event.location(in: self))
-    }
-    
-    override func mouseDragged(with event: NSEvent) {
-        self.touchMoved(toPoint: event.location(in: self))
-    }
-    
-    override func mouseUp(with event: NSEvent) {
-        self.touchUp(atPoint: event.location(in: self))
-    }
-    
+        
     override func keyDown(with event: NSEvent) {
-        switch event.keyCode {
-        case 0x31:
-            if let label = self.label {
-                label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-            }
+        switch event.charactersIgnoringModifiers?.lowercased() {
+        case "a":
+            moveLeft = true
+        case "d":
+            moveRight = true
+        case "w":
+            moveUp = true
+        case "s":
+            moveDown = true
         default:
-            print("keyDown: \(event.characters!) keyCode: \(event.keyCode)")
+            break
         }
     }
-    
-    
+    override func keyUp(with event: NSEvent) {
+        switch event.charactersIgnoringModifiers?.lowercased() {
+        case "a":
+            moveLeft = false
+        case "d":
+            moveRight = false
+        case "w":
+            moveUp = false
+        case "s":
+            moveDown = false
+        default:
+            break
+        }
+    }
     override func update(_ currentTime: TimeInterval) {
-        // Called before each frame is rendered
+        let indiecatorRadius: CGFloat = 10
+        let halfWidth = size.width / 2 - indiecatorRadius
+        let halfHeight = size.height / 2 - indiecatorRadius
+        
+        var dx : CGFloat = 0
+        var dy : CGFloat = 0
+        if moveLeft {
+            dx-=1
+        }
+        if moveRight {
+            dx+=1
+        }
+        if moveUp {
+            dy+=1
+        }
+        if moveDown {
+            dy-=1
+        }
+        let length = sqrt(dx * dx + dy * dy)
+        
+        if length > 0 {
+            dx /= length
+            dy /= length
+        }
+        let speed : CGFloat = 5
+        ball.position.x += dx * speed
+        ball.position.y += dy * speed
+        
+        let isOutside =
+        ball.position.x < -halfWidth ||
+        ball.position.x > halfWidth ||
+        ball.position.y < -halfHeight ||
+        ball.position.y > halfHeight
+        if isOutside {
+            indicator.isHidden = false
+            var x = ball.position.x
+            var y = ball.position.y
+            
+            if ball.position.x < -halfWidth {
+                x = -halfWidth
+            } else if ball.position.x > halfWidth {
+             x = halfWidth
+            }
+            
+            if ball.position.y < -halfHeight {
+                y = -halfHeight
+            } else if ball.position.y > halfHeight {
+             y = halfHeight
+            }
+            
+            //x = min(max(x, -halfWidth), halfWidth)
+            //y = min(max(y, -halfHeight), halfHeight)
+            indicator.position = CGPoint(x: x, y: y)
+        } else {
+            indicator.isHidden = true
+        }
     }
 }
